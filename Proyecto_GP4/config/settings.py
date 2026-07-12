fb0fb722bda1443c2d6d7129d26bcca6cb8f8f1a
elkin <fernandxx08@gmail.com>
2026-07-11 19:48:25 -0500

    este si es 1...

--- Proyecto_GP4/config/settings.py
+++ Proyecto_GP4/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-94-df+ga0hlu8c!12ox&vjddcy^1ssczn7*ay=x1$y+yi71$f@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"] 
 
 
 # Application definition
