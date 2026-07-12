0a59cbdce1a76eb1b761a3488e0b29868e45e95f
elkin <fernandxx08@gmail.com>
2026-07-12 16:04:55 -0500

    este si es ahora si...

--- Proyecto_GP4/config/settings.py
+++ Proyecto_GP4/config/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'la_tuna',
-        'USER': 'root',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", "3306"),
     }
 }
 
